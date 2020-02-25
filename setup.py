770325b9bbced985e2b2296091da1f9c45823280
KZARCA <kevinounet@gmail.com>
2020-02-25 20:31:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=['pandas', 'requests', 'requests-cache',
+    install_requires=['requests', 'requests-cache',
                       'six', 'munch'],  # bunch->lunch->infi.bunch->munch
 
     # List additional groups of dependencies here (e.g. development dependencies).
